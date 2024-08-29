4c3d9a194334bf7f5525bf2e1755932eb5a47025
kimathinjoki <dfkimathinjoki@gmail.com>
2024-08-28 19:37:50 -0500

    add utterances csv

--- jsonCsv.py
+++ jsonCsv.py
@@ -2,18 +2,30 @@ import json
 import csv
 import os
 
-# Function to convert JSON Lines to CSV with optional selected keys
+def print_json_keys(json_file):
+    keys_set = set()
+    
+    with open(json_file, 'r') as file:
+        for line in file:
+            json_obj = json.loads(line)
+            keys_set.update(json_obj.keys())
+    
+    print("Keys that would be columns in the CSV file:")
+    for key in keys_set:
+        print(key)
+
+
 def json_to_csv(json_file, selected_keys=None):
     data = []
     with open(json_file, 'r') as file:
         for line in file:
             data.append(json.loads(line))
     
-    # If selected_keys is not provided, use keys from the first JSON object
+    # use keys from the first JSON object
     if not selected_keys:
         selected_keys = list(data[0].keys())
     
-    # Generate CSV file name and save in 'converted_files' folder
+    # Generate CSV file name and save in converted_files folder
     base_name = os.path.basename(json_file)
     name_without_ext = os.path.splitext(base_name)[0]
     csv_file = os.path.join('converted_files', f"{name_without_ext}Csv.csv")
@@ -21,7 +33,7 @@ def json_to_csv(json_file, selected_keys=None):
     # Ensure the 'converted_files' directory exists
     os.makedirs('converted_files', exist_ok=True)
     
-    # Check if the file already exists and append a number if necessary
+    # Check if the file already exists and append a number
     if os.path.exists(csv_file):
         counter = 2
         while os.path.exists(os.path.join('converted_files', f"{name_without_ext}Csv{counter}.csv")):
@@ -35,8 +47,12 @@ def json_to_csv(json_file, selected_keys=None):
         for row in data:
             csv_writer.writerow([row.get(key, '') for key in selected_keys])
 
-# Example usage
-json_to_csv('../toBeConverted/models.jsonl')
-# json_to_csv('filename.json', ['key1', 'key2', 'key3'])
+
+# json_to_csv('../toBeConverted/models.jsonl')
+
+
+# print_json_keys('../toBeConverted/utterances.jsonl')
+
+json_to_csv('../toBeConverted/utterances.jsonl', ['model_name', 'model_provider', 'model_reponse', 'user_prompt', 'if_chosen', 'score', 'conversation_type', 'user_id'])
 
     
